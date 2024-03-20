9941f49ce3d8c08c0ccbca2c3c82ebc0b750155d
0xkanekiken <100861945+0xKanekiKen@users.noreply.github.com>
2024-03-20 10:47:55 +0000

    fix
    Signed-off-by: 0xkanekiken <100861945+0xKanekiKen@users.noreply.github.com>

--- crates/precompile/src/succinct.rs
+++ crates/precompile/src/succinct.rs
@@ -4,11 +4,11 @@ extern "C" {
 
 #[inline]
 pub fn bn254_add(p: &mut [u8; 64], q: &[u8; 64]) {
-    unsafe { syscall_bn254_add(p.as_mut_ptr(), q.as_ptr()) }
+    unsafe { syscall_bn254_add(p.as_mut_ptr() as *mut u32, q.as_ptr() as *const u32) }
 }
 
 // Convert a u8 array to a u32 array
-pub fn u8_to_u32(arr: &[u8; 64]) -> [u32; 16] {
+pub fn u8_to_u32(arr: &[u8; 32]) -> [u32; 8] {
     let mut res = [0u32; 8];
     for i in 0..8 {
         res[i] = u32::from_le_bytes(arr[i * 4..(i + 1) * 4].try_into().unwrap());
